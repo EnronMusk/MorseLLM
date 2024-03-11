06f2237edf862b749a2427a6a1ea60b503b82d79
EnronMusk <142634045+EnronMusk@users.noreply.github.com>
2024-03-10 23:33:44 -0400

    first commit

---
+++ data/tokenizer.py
@@ -0,0 +1,16 @@
+
+chars = " .-"
+
+def tokenize(string : str) -> list[int]:
+
+    encode_mapping = { ch:i for i,ch in enumerate(chars)}
+    encode = lambda string : [encode_mapping[c] for c in string]
+
+    return encode(string)
+
+def detokenize(string : str) -> list[int]:
+
+    decode_mapping = { i:ch for i,ch in enumerate(chars)}
+    decode = lambda string : ''.join([decode_mapping[c] for c in string])
+
+    return decode(string)
